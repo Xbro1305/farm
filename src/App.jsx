c7fe6e9d68c4afe2196aa295247eab44460bfed9
xbro1305 <x.bro2010@gmail.com>
2024-06-17 22:25:41 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import logo from "./images/logo.svg";
+import main from "./images/main.svg";
+import energy from "./images/energy.svg";
+import first from "./images/Frame 25.svg";
+import second from "./images/Frame 25 (1).svg";
+import third from "./images/Frame 25 (2).svg";
+import fourth from "./images/Frame 25 (3).svg";
+import fifth from "./images/Frame 25 (4).svg";
+import league from "./images/league.svg";
+import group from "./images/Group 1000001775.svg";
+
+export const App = () => {
+  return (
+    <div className="main">
+      <h1>
+        <p>
+          <img src={logo} alt="" /> 349.917 B
+        </p>
+        <span>
+          <img src={league} alt="" /> Wooden League
+        </span>
+      </h1>
+      <img src={main} alt="" />
+      <div className="footer">
+        <div>
+          <section>
+            <h1>
+              <img className="energy" src={energy} alt="" />
+              1500/1500
+            </h1>
+            <span>Energy</span>
+          </section>
+          <img src={group} alt="" />
+        </div>
+        <div className="navigation">
+          <a href="">
+            <img src={first} alt="" />
+            FARM
+          </a>
+          <a href="">
+            <img src={second} alt="" />
+            EARN
+          </a>
+          <a href="">
+            <img src={third} alt="" />
+            TAP
+          </a>
+          <a href="">
+            <img src={fourth} alt="" />
+            BOOST
+          </a>
+          <a href="">
+            <img src={fifth} alt="" />
+            STATS
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
